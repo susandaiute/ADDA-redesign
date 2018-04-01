5149d0043eaac93ab9c50b730cee5e5be4ce20e8
susandaiute <susandaiute@gmail.com>
2018-04-01 18:04:33 -0400

    Fix grunt server

---
+++ Gruntfile.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  grunt.loadNpmTasks('grunt-serve');
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  grunt.loadNpmTasks('grunt-bootlint');
+
+
+};
